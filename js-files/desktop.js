32e4015e4f52d5d1c4c9e8cf086ffee7639bfc39
Albin-AW <albin.moller@elev.ga.lbs.se>
2025-10-14 11:03:16 +0200

    fixed a bit

--- js-files/desktop.js
+++ js-files/desktop.js
@@ -2,7 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector("#container"),  
           tile = document.querySelector(".tile")
 
-    for (let i = 0; i < 1599; i++) { 
-        container.appendChild(tile.cloneNode())
-    }
+    for (let i = 0; i < 1599; i++) { container.appendChild(tile.cloneNode()) }
 })
